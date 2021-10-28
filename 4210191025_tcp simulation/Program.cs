31ab3eb1eb798cc903c2571fe09d1585ec847a38
nindyasaff <63599760+nindyasaff@users.noreply.github.com>
2021-10-29 01:33:51 +0700

    Add files via upload

---
+++ 4210191025_tcp simulation/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _4210191025_tcp_simulation
+{
+    class Client
+    {
+        Console.WriteLine("Enter the server IP address: ");
+        string serverIp = Console.ReadLine();
+        Console.WriteLine("Enter the server Port number: ");
+        int serverPort = int.Parse(Console.ReadLine());
+
+        // Create the endpoint for the server
+        IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse(serverIp), serverPort);
+        // Create a TCP socket to send data over
+        Socket sendSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+ 
+        try{
+
+            // Connect to the server
+            sendSocket.Connect(endPoint);
+            // Start looping for multiple messages
+            
+            while (true){
+            Console.WriteLine("Enter the message to send: ");
+            string message = Console.ReadLine() + "\n";
+ 
+                if (message.Equals("EXIT\n")){
+                // Close socket connection to the server
+                sendSocket.Close();
+                break;
+            }
+
+            else{
+                // Convert the string message to a byte array with ASCII encoding
+                byte[] sendMessage = Encoding.ASCII.GetBytes(message);
+    // Send the data to the server over the TCP socket
+    sendSocket.SendTo(sendMessage, endPoint);
+            }
+        }
+    
+        Console.WriteLine("Client shutting down.");
+    }
+
+
+        catch (Exception e)
+{
+    Console.WriteLine(e.ToString());
+}
---
+++ 4210191025_tcp simulation/Server.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class Class1
+{
+    class Server
+    {
+        // Create the TCP listener that will accept incoming connection requests
+        TcpListener listener = new TcpListener(IPAddress.Parse(GetIpAddress()), 6000);
+        // Start listening
+        listener.Start();
+        Console.WriteLine("Server listening on " + listener.LocalEndpoint);
+ 
+        // Create the server socket that will connect to the client
+        Socket socket = listener.AcceptSocket();
+        // Create a byte array to hold the incoming data
+        byte[] receivedBytes = new byte[1500];
+ 
+        try{
+        while (true){
+        // Receive the incoming byte array over the server socket
+        int receivedSize = socket.Receive(receivedBytes);
+ 
+        for(int i=0; i&lt;receivedSize; i++){
+        // Print each character to the console window
+        Console.Write(Convert.ToChar(receivedBytes[i]));
+        }
+}
+        }
+    catch (Exception e)
+{
+    Console.WriteLine(e.ToString());
+}
+
+
